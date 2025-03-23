7c4e2813aaf079af556075bb2026fe3b1ea44e66
pash masr <129723189+pashamasr01287654800@users.noreply.github.com>
2025-03-23 08:35:54 +0200

    Update macchanger_wlan0.sh

--- macchanger_wlan0.sh
+++ macchanger_wlan0.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
 
 # Disable the network interface
-ifconfig wlan0 down
+sudo ifconfig wlan0 down
 
 # Change the MAC address
-macchanger -a wlan0
+sudo macchanger -a wlan0
 
 # Enable the network interface
-ifconfig wlan0 up
-
-# Check if 'service' command is available and restart the network manager accordingly
-if command -v service &> /dev/null; then
-    systemctl restart NetworkManager
-else
-    service network-manager restart
-fi
+sudo ifconfig wlan0 up
